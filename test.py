2ef94cdb4bf225dc8ddde1ea294da4b2f8225275
Никита <nervok1337@gmail.com>
2026-02-10 14:51:31 +0800

    Added test for list division

--- test.py
+++ test.py
@@ -12,6 +12,14 @@ def test_devide():
     result = 0.5
     assert devide(a,b) == result
 
+def test_devision_prohibited():
+    try:
+        devide([1,2,3],[4,5,6])
+        print("Test list-division failed")
+        assert False
+    except ValueError as e:
+        print("Test list-division passed")
+
 def test_devide_zero():
     a = 2
     b = 0
