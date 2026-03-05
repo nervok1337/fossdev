8b5788fefec4bea6cec63318ccbd10392bc7a754
Никита <nervok1337@gmail.com>
2026-03-05 12:56:39 +0800

    show test-may-not-show errors

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -1,10 +1,27 @@
 #TODO make it with `pip install -e .`
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
+
+# Ранее тестирование позволяет сэкономить время позднее
+# 
+
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(7, 6) == 13
     print("Test addition passed")
+
+def test_addition_with_bug():
+    assert add_with_bug(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    print("test bugged addition passed")
+    # finally we found data that make test reliable
+    # assert add_with_bug(7,6) == 13 # will fail here 
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
