edb56ce1a18065c939532a12e51ff64f5251fd83
Никита <nervok1337@gmail.com>
2026-03-05 13:04:58 +0800

    added demo for do-not-duplicate-internal-logic principle

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -5,9 +5,16 @@ from math_demo import (
     add_with_bug
 )
 
-# Ранее тестирование позволяет сэкономить время позднее
-# 
+# [DONE] Ранее тестирование позволяет сэкономить время позднее
+# [DONE] Тесты показывают наличие ошибок, а не их отсутствие
 
+# [DONE] Тесты не должны дублировать логику тестируемого кода
+# Тесты не должны делать предположения о внутреннем устройстве кода
+
+# Тесты не должны использовать ВСЕ наборы входных параметров
+# Тесты должны покрывать "кластеры" входных параметров
+# Тесты должны обнаруживать ошибки (perscide paradox)
+# Тесты покрывают как успешные так и ошибочные кейсы
 
 def test_addition():
     assert add(2, 2) == 4
@@ -22,6 +29,11 @@ def test_addition_with_bug():
     # finally we found data that make test reliable
     # assert add_with_bug(7,6) == 13 # will fail here 
 
+def test_addition_duplicate():
+    assert add(6,7) == 6+7
+    print("test duplicate addition passed")
+
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicate()
